bd8e15ef021ba9021e0537e24bee48954a8ddc59
tang <donquixote1110@outlook.com>
2024-09-11 17:42:51 +0800

    init

---
+++ app.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const views = require("koa-views");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+// 配置模板引擎（EJS）
+app.use(views(path.join(__dirname, "views"), { extension: "ejs" }));
+
+// 配置路由
+router.get("/", async (ctx) => {
+  await ctx.render("index", { title: "AI Store", apiKey: process.env.API_KEY });
+});
+router.post("/api/generate", async (ctx) => {
+  const { prompt } = ctx.request.body;
+  if (!prompt) {
+    ctx.status = 400;
+    ctx.body = { message: "Prompt is required" };
+    return;
+  }
+
+  try {
+    const command = `python3 py/tts_inference.py "${prompt}"`;
+
+    console.log(`Executing command: ${command}`);
+
+    const { stdout, stderr } = await exec(command);
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      ctx.status = 500;
+      ctx.body = { message: "TTS processing failed", data: stderr };
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+
+    // 读取生成的音频文件
+    const audioPath = path.join(__dirname, "py/output.wav");
+
+    // 检查音频文件是否存在
+    if (!fs.existsSync(audioPath)) {
+      ctx.status = 500;
+      ctx.body = { message: "Audio file not generated" };
+      return;
+    }
+
+    // 返回音频文件
+    const audioData = fs.readFileSync(audioPath);
+    ctx.set("Content-Type", "audio/wav");
+    ctx.body = audioData;
+  } catch (error) {
+    console.error(`exec error: ${error}`);
+    ctx.status = 500;
+    ctx.body = { message: "TTS processing failed" };
+  }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3003, () => {
+  console.log("server is running at http://localhost:3003");
+});
---
+++ py/tts_inference.py
@@ -0,0 +1,32 @@
+from transformers import pipeline
+import sys
+import os
+import soundfile as sf
+
+
+os.environ["HF_HUB_ENABLE_SYMLINKS"] = "false"
+def text_to_speech(text):
+    # 加载 hugging face TTS 模型
+    tts_pipeline = pipeline("text-to-speech", model="suno/bark-small")
+
+    # 进行推理，生成语音
+    audio = tts_pipeline(text)
+    
+
+    # 获取音频数据和采样率
+    audio_data = audio['audio'].flatten()
+    sampling_rate = audio['sampling_rate']
+
+    # 保存为 WAV 文件
+    sf.write('output.wav', audio_data, sampling_rate)
+    
+    # # 保存语音文件
+    # with open("output.wav", "wb") as f:
+    #     f.write(audio["wav"])
+
+
+if __name__ == "__main__":
+    text = sys.argv[1]  # 从命令行参数获取输入文本
+    print("输入文本：", text)
+    text_to_speech(text)
+    print("语音生成完成！")
