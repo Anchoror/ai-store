b7cf411dc9687ad1b26d68ccd36b51292374c966
tangjiacheng <donquixote1110@outlook.com>
2024-12-12 18:47:23 +0800

    init

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const bodyParser = require("koa-bodyparser");
 const views = require("koa-views");
 const path = require("path");
 const router = require("./routes");
+const serve = require("koa-static");
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(views(path.join(__dirname, "views"), { extension: "ejs" }));
 app.use(router.routes()).use(router.allowedMethods());
+app.use(serve(path.join(__dirname, "static")));
 
 app.listen(3004, () => {
   console.log("server is running at http://localhost:3004");
--- services/itvService.js
+++ services/itvService.js
@@ -2,43 +2,51 @@ const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
 const path = require("path");
 const { getDb, ObjectId } = require("./dbService");
+const https = require("https");
+const axios = require("axios");
 
 const command = ffmpeg();
 
-const mix = async (id) => {
-  const db = await getDb();
-  const data = await db
-    .collection("aiRes")
-    .findOne({ _id: new ObjectId("6757f17fbc49e31fdc944a7d") });
-  console.log("data", id, data);
-  const { audio: audioArr, img: imgArr, text: textArr } = data.result;
+const downloadFile = async (url, dest) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: url,
+      responseType: "stream", // 设置响应类型为流
+    });
 
-  const tempDir = path.join(__dirname, "../static/temp");
-  if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir);
+    const writer = fs.createWriteStream(dest);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  } catch (error) {
+    // fs.unlink(dest);
+    console.error("下载图片时出错:", error.message);
+
+    throw error;
   }
+};
 
-  const tempVideoFiles = [];
-
-  for (let i = 0; i < audioArr.length; i++) {
-    const audioPath = audioArr[i];
-    const imgPath = imgArr[i];
-    const text = textArr[i];
-
-    const tempVideoPath = path.join(tempDir, `temp_video_${i}.mp4`);
-    tempVideoFiles.push(tempVideoPath);
-
-    // 创建临时视频
-    command
-      .input(imgPath)
+const createVideoFromImageAndAudio = (
+  audioPath,
+  imagePath,
+  text,
+  fontPath,
+  outputPath
+) => {
+  const arr = text.split("");
+  arr.splice(18, 0, "\n");
+  const drawtext = arr.join("");
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .input(imagePath)
+      .inputOptions("-loop", "1")
       .input(audioPath)
-      .inputOptions([
-        "-loop",
-        "1", // 循环播放图片
-
-        "-shortest", // 以最短的输入为准
-      ])
-      .outputOptions([
+      .outputOptions(
         "-c:v",
         "libx264",
         "-preset",
@@ -47,15 +55,75 @@ const mix = async (id) => {
         "23",
         "-pix_fmt",
         "yuv420p",
+        "-shortest"
+      )
+      .videoFilters([
+        {
+          filter: "drawtext",
+          options: {
+            text: drawtext,
+            fontsize: 48,
+            fontcolor: "white",
+            x: "(w-text_w)/2", // 居中显示
+            y: "(h-text_h)*0.88", // 居中显示
+            box: 1,
+            boxcolor: "black@0.5",
+            boxborderw: 5,
+            fontfile: fontPath,
+          },
+        },
       ])
-      .output(tempVideoPath)
+      .output(outputPath)
       .on("end", () => {
-        console.log(`临时视频${i}生成完毕`);
+        console.log("视频生成完成----", audioPath, imagePath, outputPath);
+        resolve(outputPath);
       })
       .on("error", (err) => {
-        console.error(`临时视频${i}生成出错:`, err);
+        console.error("视频生成出错:", err);
+        reject(err);
       })
       .run();
+  });
+};
+
+const mix = async (id) => {
+  const db = await getDb();
+  const data = await db.collection("aiRes").findOne({ _id: new ObjectId(id) });
+  console.log("data", data);
+  const { audio: audioArr, img: imgArr, text: textArr } = data.result;
+
+  const tempDir = path.join(__dirname, "../static/temp");
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir);
+  }
+
+  const tempVideoFiles = [];
+  console.log("tempDir", tempDir, audioArr.length);
+
+  for (let i = 0; i < audioArr.length; i++) {
+    const audioPath = audioArr[i];
+    const imgPath = imgArr[i];
+    const text = textArr[i];
+
+    const tempAudioPath = path.join(tempDir, `temp_audio_${i}.mp3`);
+    const tempImgPath = path.join(tempDir, `temp_img_${i}.png`);
+    const tempVideoPath = path.join(tempDir, `temp_video_${i}.mp4`);
+    const fontPath = path.join(__dirname, "../static/font/font.otf");
+    tempVideoFiles.push(tempVideoPath);
+
+    // 下载远程文件到本地
+    console.log("audioPath", audioPath, imgPath);
+    await downloadFile(audioPath, tempAudioPath);
+    await downloadFile(imgPath, tempImgPath);
+
+    // // 创建临时视频
+    createVideoFromImageAndAudio(
+      tempAudioPath,
+      tempImgPath,
+      text,
+      fontPath,
+      tempVideoPath
+    );
   }
 
   // 合并所有视频
@@ -67,25 +135,28 @@ const mix = async (id) => {
 
   outputStream.end();
 
-  const outputPath = path.join(__dirname, "../static/itv/output.mp4");
-  command
-    .input(concatFilePath)
-    .inputOptions(["-f", "concat", "-safe", "0"])
-    .output(outputPath)
-    .on("end", () => {
-      console.log("最终视频合并完成");
-      // 清理临时文件
-      tempVideoFiles.forEach((filePath) => {
-        fs.unlinkSync(filePath);
-      });
-      fs.unlinkSync(concatFilePath);
-    })
-    .on("error", (err) => {
-      console.error("最终视频合并出错:", err);
-    })
-    .run();
+  const outputPath = path.join(__dirname, "../static/itv/concat_video.mp4");
 
-  return outputPath;
+  return new Promise((resolve, reject) => {
+    command
+      .input(concatFilePath)
+      .inputOptions(["-f", "concat", "-safe", "0"])
+      .output(outputPath)
+      .on("end", () => {
+        console.log("最终视频合并完成");
+        resolve(outputPath);
+        // 清理临时文件
+        // tempVideoFiles.forEach((filePath) => {
+        //   fs.unlinkSync(filePath);
+        // });
+        // fs.unlinkSync(concatFilePath);
+      })
+      .on("error", (err) => {
+        console.error("最终视频合并出错:", err);
+        reject(err);
+      })
+      .run();
+  });
 };
 
 module.exports = {
